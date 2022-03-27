2b3981bf20bee173f15dc3aa788a1bdd60f4b766
Alec DiFederico <alecdifederico@gmail.com>
2022-03-27 16:56:12 -0400

    added comments

--- app/translator.py
+++ app/translator.py
@@ -31,24 +31,39 @@ class DiscordTranslator:
                 "op": 1,
                 "d": "null"
             }
-            self._send_json_request(heartbeat_json)
+
+            try: 
+                self._send_json_request(heartbeat_json)
+            except ConnectionResetError:
+                print('[bold red]WARNING: Connection was reset by Discord. Reconnecting the socket...[/bold red]')
+                self.initiate_connection()
+                print('[bold green]Socket was successfully reset. Resuming translation...[/bold green]')
+            except:
+                pass
 
     def _display_message(self, message):
         username = message['author']['username']
         message_text = message['content']
+
+        # detect the language of the text
         detection = self.translator.detect(message_text)
 
+        # if the text is in chinese, then translate it
         if detection.lang == 'zh-CN':
             message_text = self.translator.translate(message_text).text
 
         print(f"[bold yellow]{username}[/bold yellow]: {message_text}")
 
     def initiate_connection(self):
+
+        # connection the websocket to the discord gateway
         self.ws = websocket.WebSocket()
         self.ws.connect(self.config.DISCORD_GATEWAY)
 
+        # create a google translator object
         self.translator = Translator()
 
+        # begin the heartbeat thread
         event = self._receive_json_response()
         heartbeat_interval = event['d']['heartbeat_interval'] / 1000
         threading._start_new_thread(self._heartbeat, (heartbeat_interval, ))
@@ -64,6 +79,8 @@ class DiscordTranslator:
                 }
             }
         }
+
+        # send the initial connection payload
         self._send_json_request(payload)
 
     def load_initial_messages(self):
@@ -75,6 +92,7 @@ class DiscordTranslator:
             'limit': 10
         }
 
+        # retrieve the 10 most recent messages from the channel
         response = requests.get(self.config.DISCORD_API, headers=headers, params=options)
         messages = json.loads(response.text)
         for message in reversed(messages):
@@ -84,14 +102,15 @@ class DiscordTranslator:
         while True: 
             response = self._receive_json_response()
             response_content = response['d']
-
+            
             try: 
+                # only retrieve messages from the desired discord channel
                 if response_content['channel_id'] == self.config.CHANNEL_ID:
                     self._display_message(response_content)
 
             except ConnectionResetError:
-                print('connection reset error. Restarting socket connection...')
+                print('[bold red]WARNING: Connection was reset by Discord. Reconnecting the socket...[/bold red]')
                 self.initiate_connection()
-                print('connection restarted')
+                print('[bold green]Socket was successfully reset. Resuming translation...[/bold green]')
             except:
                 pass
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app.translator import DiscordTranslator
 from config import BaseConfig
 
+# create an instance of the discord translator
 discordTranslator = DiscordTranslator(BaseConfig)
 
+# run the translator
 discordTranslator.initiate_connection()
 discordTranslator.load_initial_messages()
 discordTranslator.load_realtime_messages()
